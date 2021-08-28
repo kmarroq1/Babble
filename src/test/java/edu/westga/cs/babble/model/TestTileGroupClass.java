90a0b68943b19ad35d6da5950ef7061cc85e20cd
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-08-27 23:11:28 -0400

    Tested append method from TileGroup class
    and created test class that extends TileGroup abstract class

---
+++ src/test/java/edu/westga/cs/babble/model/TestTileGroupClass.java
@@ -0,0 +1,12 @@
+package edu.westga.cs.babble.model;
+
+/**
+ * This class inherits from the TileGroup abstract class and is used for testing
+ * purposes.
+ * 
+ * @author Kimberly Marroquin
+ * @version Aug 27, 2021
+ */
+public class TestTileGroupClass extends TileGroup {
+
+}
